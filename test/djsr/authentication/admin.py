79d9b631af5cc59bb987077497880807632fc995
Mark Harmon <neothej1sec@gmail.com>
2022-05-05 12:17:16 -0500

    added test backend to work on django auth

---
+++ test/djsr/authentication/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import CustomUser
+# Register your models here.
+class CustomUserAdmin(admin.ModelAdmin):
+    model = CustomUser
+admin.site.register(CustomUser, CustomUserAdmin)
