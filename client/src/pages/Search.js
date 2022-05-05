16cdf91e25c40d4021aab7f588b406822d9a0e06
Mark Harmon <neothej1sec@gmail.com>
2022-05-05 13:20:27 -0500

    added search user page functionality

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,8 +1,42 @@
-
+import React, {useState, useEffect} from 'react'
+import { GetUsers } from '../services/UserServices'
 
 const Search = () => {
+    const [users, setUsers] = useState([])
+    const [query, setQuery] = useState('')
+    const [search, setSearch] = useState('')
+
+    useEffect(() => {
+        const getUsers = async () => {
+            const all =  await GetUsers()
+            setUsers(all)
+        }
+        getUsers()
+    }, [])
+
+    const handleChange = (e) => {
+        setQuery(e.target.value)
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const res = users.filter((item) => item.username.toLowerCase().includes(search.toLowerCase()))
+        setSearch(res)
+        console.log("RES", res)
+    }   
+
     return(
-        <div></div>
+        <div className='search'>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    className='search-user-bar'
+                    onChange={(e) => handleChange(e)}
+                    value={query}
+                    maxLength='255'
+                    name='username'
+                    placeholder='Search Friends..'
+                />
+            </form>
+        </div>
     )
 }
 
