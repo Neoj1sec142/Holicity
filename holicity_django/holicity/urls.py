1f06f9009e2874cd425d6760cce014f23c406ed7
Mark Harmon <neothej1sec@gmail.com>
2022-05-05 09:15:00 -0500

    added urls and views

---
+++ holicity_django/holicity/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+from rest_framework.routers import DefaultRouter
+
+urlpatterns = [
+  path('users/', views.UserList.as_view(), name='user_list'),
+  path('users/<int:pk>', views.UserDetail.as_view(), name='user_detail'),
+  path('followers/', views.FollowerList.as_view(), name='follower_list'),
+  path('followers/<int:pk>', views.FollowerDetail.as_view(), name='follower_detail'),
+  path('following/', views.FollowingList.as_view(), name='following_list'),
+  path('following/<int:pk>', views.FollowingDetail.as_view(), name='following_detail'),
+  path('posts/', views.PostList.as_view(), name='post_list'),
+  path('posts/<int:pk>', views.PostDetail.as_view(), name='post_detail'),
+  path('comments/', views.CommentList.as_view(), name='comment_list'),
+  path('comments/<int:pk>', views.CommentDetail.as_view(), name='comment_detail')
+]
