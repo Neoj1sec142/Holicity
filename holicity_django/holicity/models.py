6ade032d9cbdbb8fde7f37810e3a9c5efe1d2438
Mark Harmon <neothej1sec@gmail.com>
2022-05-04 22:12:31 -0500

    successfully created user, follower, following, post, and comment models and successfully migrated the to DB

--- holicity_django/holicity/models.py
+++ holicity_django/holicity/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    profile_img = models.TextField()
+    email = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+    
+class Follower(models.Model):
+    name = models.CharField(max_length=100)
+    users = models.ForeignKey(User, on_delete=models.CASCADE, related_name="followers")
+    def __str__(self):
+        return self.name
+
+class Following(models.Model):
+    name = models.CharField(max_length=100)
+    users = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    type = models.CharField(max_length=50)
+    title = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    related_links = models.CharField(max_length=500)
+    post_img = models.TextField()
+    description = models.CharField(max_length=2500)
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    rating = models.IntegerField(null=True)
+    description = models.CharField(max_length=1500)
+    def __str__(self):
+        return self.user
