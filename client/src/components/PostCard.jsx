85564ff670757341671791ec3115a9e50ce1184d
Mark Harmon <neothej1sec@gmail.com>
2022-05-10 13:17:39 -0500

    styled feed a little more

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -1,13 +1,14 @@
 import { useNavigate } from 'react-router-dom'
-
+import { Card} from 'react-bootstrap'
 const PostCard = ({post}) => {
 const navigate = useNavigate();
 
     return (
         <div className='post-card-container' key={post.id} onClick={() => navigate(`/details/${post.id}`)}>
-                <h3>{post.title}</h3>
+            <Card>
+                <h3 className='btn btn-outline-warning'>{post.title}</h3>
                 <div className='post-card-img' style={{backgroundImage:`url(${post.image})`}}></div>
-
+            </Card>
         </div>
     )
 }
--- client/src/pages/Feed.js
+++ client/src/pages/Feed.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import { GetPosts } from '../services/PostServices'
 import PostCard from '../components/PostCard.jsx'
-// import {Alert, Button, Breadcrumb, Card} from 'react-bootstrap'
+import { Card} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 
@@ -22,22 +22,26 @@ const Feed = (props) => {
     if(feed.length){
     return (
         <div className='feed'>
+            <Card>
             {feed.sort((a,b) => {return (a.updatedAt < b.updatedAt) ? 1 : -1} ).map((post, i) => (
-                <div key={i} className='post-container'>
+                <Card.Body key={i} className='post-container'>
                     <div className='username' onClick={() => navigate(`/profile/${post.userId}`)} >
                     {post.User.profileImg 
-                        ?   <img src={`${post.User.profileImg}`} alt='profile-img' style={{maxWidth: '150px'}}/>
-                        :   <img src='../assets/no_img.jpeg' alt='no-profile-img'/>
+                        ?   <img src={`${post.User.profileImg}`} className="d-flex justify-content-start"
+                        alt='profile-img' style={{maxWidth: '50px'}}/>
+                        :   <img src='../assets/no_img.jpeg' className="d-flex justify-content-center"
+                        alt='no-profile-img'/>
                     // <div className='profile-img-container username-stripe-img' style={{backgroundImage:`url(${feed.User.profileImg})`}}></div> 
                     // :
                     // <div className='profile-img-container username-stripe-img'></div>
                     }
-                    {post.User.username}
+                    <Card.Title className='d-flex align-items-top'>{post.User.username}</Card.Title>
                     </div>
                     
                     <PostCard post={post} />
-                </div>
+                </Card.Body>
             ))}
+            </Card>
         </div>
     )
     }else{
