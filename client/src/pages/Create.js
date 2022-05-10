ca14752c378b5ca42255057c31029d5c4ca84e08
Mark Harmon <neothej1sec@gmail.com>
2022-05-10 08:52:12 -0500

    added some btstp to create page

--- client/src/pages/Create.js
+++ client/src/pages/Create.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import {useNavigate} from 'react-router-dom'
 import {CreatePost} from '../services/PostServices'
-
+import {Card} from 'react-bootstrap'
 
 const Create = (props) => {
     const navigate = useNavigate()
@@ -39,9 +39,10 @@ const Create = (props) => {
 
     return(
         <div className='create'>
+           <Card className="position-absolute top-50 start-50 translate-middle" style={{ padding: '20px', maxWidth: '500px'}}>
             <form className='card-overlay centered' onSubmit={handleSubmit}>
-                <h3>~ Spread the Energy ~</h3>
-                <div className='input-wrapper'>
+                <h3 className='d-flex justify-content-center' style={{paddingBottom: '15px'}}>~ Spread the Energy ~</h3>
+                <div className='d-flex justify-content-center' style={{paddingBottom: '15px'}}>
                     <h4>~ Name your Vibe ~</h4>
                     <input 
                         onChange={handleChange}
@@ -53,9 +54,10 @@ const Create = (props) => {
                         required
                     />
                 </div>
-                <div className='input-wrapper'>
-                    <h4>~ What is your Vibe ~</h4>
-                    <div>
+                <div className=''>
+                    
+                    <div className='d-flex  align-items-center'>
+                        <h4 className='' style={{paddingTop: '5px'}}>~ Type: ~</h4>
                         <label htmlFor='type'></label>
                         <select onChange={(e) => handleSelect(e)} value={post.type} name='type' id='type'>
                             {allTypes.map((type, i) => (
@@ -65,7 +67,7 @@ const Create = (props) => {
                         </select>
                     </div>
                 </div>
-                <div className='input-wrapper'>
+                <div className='d-flex justify-content-center align-items-center' style={{padding: '10px'}}>
                     <h4>~ Share a Pic ~</h4>
                     <textarea 
                         onChange={handleChange}
@@ -76,7 +78,7 @@ const Create = (props) => {
                         maxLength='255'
                     />
                 </div>
-                <div className='input-wrapper'>
+                <div className='d-flex justify-content-center align-items-center' style={{padding: '10px'}}>
                     <h4>~ Pass on Wisdom ~</h4>
                     <textarea 
                         onChange={handleChange}
@@ -87,7 +89,7 @@ const Create = (props) => {
                         maxLength='255'
                     />
                 </div>
-                <div className='input-wrapper'>
+                <div className='d-flex justify-content-center align-items-center' style={{padding: '10px'}}>
                     <h4>~ Link ~</h4>
                     <input 
                         onChange={handleChange}
@@ -98,10 +100,11 @@ const Create = (props) => {
                         maxLength='255'
                     />
                 </div>
-                <div className='input-wrapper center'>
-                    <button type='submit'>Vibe</button>
+                <div className='d-flex justify-content-center'>
+                    <button type='submit' className='btn btn-outline-primary'>Vibe</button>
                 </div>
             </form>
+            </Card> 
         </div>
     )
 }
