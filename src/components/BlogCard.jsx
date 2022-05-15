a9dd8e371993c72abd713c15d5c184339636efac
Mark Harmon <neothej1sec@gmail.com>
2022-05-15 11:40:53 -0500

    working on blog bugs

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -7,18 +7,21 @@ const BlogCard = ({blog, user}) => {
         await RemoveBlog(id)
         window.location.reload(true)
     }
-    return (
-        <div className='d-flex justify-content-center' key={blog.id}>
-            <Card style={{maxWidth: '60%'}}>
-                <Card.Title  className='d-flex justify-content-center'>{blog.type}</Card.Title>
-                <Card.Text style={{textAlign: 'center'}}>{blog.thoughts}</Card.Text>
-                <a href={`${blog.url}`} style={{textAlign: 'center'}}>{blog.url}</a>
-                {blog.userId === user.id
-                ? <Button onClick={() => handleRemove(blog.id)}>X</Button>
-                : <div></div>}
-            </Card>
-        </div>
-    )
+    // console.log(blog,"BLOG")
+    if (user && blog){
+        return (
+            <div className='d-flex justify-content-center' key={blog.id}>
+                <Card style={{maxWidth: '60%'}}>
+                    <Card.Title  className='d-flex justify-content-center'>{blog.type}</Card.Title>
+                    <Card.Text style={{textAlign: 'center'}}>{blog.thoughts}</Card.Text>
+                    <a href={`${blog.url}`} style={{textAlign: 'center'}}>{blog.url}</a>
+                    {blog.userId === user.id
+                    ? <Button onClick={() => handleRemove(blog.id)}>X</Button>
+                    : <div></div>}
+                </Card>
+            </div>
+        )
+    }
 }
 
 export default BlogCard
