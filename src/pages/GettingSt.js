3ba41d637e5e359b63c9fb71aaf35b9a59344a7c
Mark Harmon <neothej1sec@gmail.com>
2022-05-16 08:21:08 -0500

    gettingst mess

--- src/pages/GettingSt.js
+++ src/pages/GettingSt.js
@@ -9,11 +9,13 @@ const Getting = (props) => {
                 ?   <Card.Title>~ Welcome to our family {props.user.username}! ~</Card.Title>
                 :   <Card.Title>~ Welcome to our family! ~</Card.Title>}
                 <Card.Text>
-                    You just being here gives the world hope. Holicity is a social platform for people 
-                    to commune and reminsce about hacks, tips, tricks, and any ways possible that we can
-                    contribute to helping restore the health of our home. As with being a site thats built 
-                    for improvement, there is a type of post or blog you may choose to use to suggest ways
-                    to improve the app. Any and all suggestions are appreciated as we wish to make the app
+                    You just being here gives the world hope. Welcome to Holicity, a holistic approach to
+                    a greener earth. Holicity social platform for people to commune, share, and reminsce 
+                    about hacks, tips, tricks, and any ways possible that we can contribute to helping 
+                    restore the health of our home. As with being a site thats built for improvement, 
+                    there is a type of post or blog you may choose to use to suggest ways to improve the app 
+                    as well as a link to contact Holicity and offer any suggestions or ideas. 
+                    Any and all suggestions are appreciated as we wish to make the app
                     as easily accessable and readliy avaliable to any and all-comers. So please feel free
                     to let me know what you would like to see... It is a site you use after all...
                 </Card.Text>
